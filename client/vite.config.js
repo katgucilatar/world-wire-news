44bd54a12143a6f3664b4545d31ef5c8bd0ab2e7
Ilir <ilir.hajdari111@gmail.com>
2023-08-21 23:20:23 -0400

    added postcss config to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
     port: 3000,
     open: true,
     proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
+      "/graphql": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
+  },
+});
