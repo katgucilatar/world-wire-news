51995f50296797f0f05224c43256ad861265ceab
Karmen <katgucilatar@outlook.com>
2023-08-24 19:46:07 -0400

    added auth and localStorage to client side

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { Link, useNavigate } from "react-router-dom";
 
-import { LOGIN_USER } from "../graphql/mutations";
+import { LOGIN_USER } from "../utils/mutations";
 
 import { useCurrentUserContext } from "../context/CurrentUser";
 
---
+++ client/src/utils/localStorage.js
@@ -0,0 +1,30 @@
+export const getSavedNewsIds = () => {
+    const savedNewsIds = localStorage.getItem('saved_news')
+      ? JSON.parse(localStorage.getItem('saved_news'))
+      : [];
+  
+    return savedNewsIds;
+  };
+  
+  export const saveNewsIds = (newsIdArr) => {
+    if (newsIdArr.length) {
+      localStorage.setItem('saved_news', JSON.stringify(newsIdArr));
+    } else {
+      localStorage.removeItem('saved_news');
+    }
+  };
+  
+  export const removeNewsId = (newsId) => {
+    const savedNewsIds = localStorage.getItem('saved_news')
+      ? JSON.parse(localStorage.getItem('saved_news'))
+      : null;
+  
+    if (!savedNewsIds) {
+      return false;
+    }
+  
+    const updatedSavedNewsIds = savedNewsIds?.filter((savedNewsId) => savedNewsId !== newsId);
+    localStorage.setItem('saved_news', JSON.stringify(updatedSavedNewsIds));
+  
+    return true;
+  };
