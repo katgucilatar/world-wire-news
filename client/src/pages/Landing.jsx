54c27fe30dfd1a3f40d00947f70088135b037463
Herb Sneed <hmsneed@gmail.com>
2023-08-24 20:15:59 -0400

    group commit

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,45 +1,39 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getHeadlines } from '../utils/news-api';
-import { dummyNewsItems } from '../../../_misc/hs_dummyData'
+import { dummyNewsItems } from '../../../_misc/hs_dummyData';
 
 const Landing = () => {
- const [newsItems, setNewsItems] = useState([]);
-  
- useEffect(() => {  
-  const fetchNews = async () => {
-    try {
-      const response = await getHeadlines();
+  const [newsItems, setNewsItems] = useState([]);
 
-      if (!response.ok) {
-      throw new Error('something went wrong!');
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        // const response = await getHeadlines();
+
+        // if (!response.ok) {
+        //   throw new Error('something went wrong!');
+        // }
+
+        // const formatted = await response.json();
+        // const { items } = formatted;
+
+        // const newsData = dummyNewsItems.map((news) => ({
+        //   newsId: news.id,
+        //   title: news.title,
+        //   source_country: news.source_country,
+        //   image: news.image,
+        //   url: news.url,
+        // }));
+
+        setNewsItems(newsData);
+      } catch (err) {
+        console.error(err);
       }
+    };
 
-      const formatted = await response.json();
-      const { items } = formatted;
-
-      console.log(formatted)
-
-      const newsData = items.map((news) => ({
-      newsId: news.id,
-      title: news.title,
-      source_country: news.source_country,
-      image: news.image,
-      url: news.url,
-      }));
-
-      console.log(newsData)
-
-      // setNewsItems(newsData);
-      setNewsItems(dummyNewsItems);
-    } catch (err) {
-      console.error(err);
-    }
-    }
-      fetchNews();
- 
+    fetchNews();
   }, []);
 
-  {newsItems.map((news) => {
   return (
   <>
   
@@ -47,43 +41,19 @@ const Landing = () => {
     <h1 className="page-title">Headlines</h1>
    </header>
 
-   <section id="top-five-hl" className="grid grid-cols-1 gap-y-2 px-2">
-    <div>
-      <img></img>
-      <h3>{news.title}</h3>
-    </div>
-
-    <div>
-      <img></img>
-      <h3></h3>
-    </div>
-
-    <div>
-      <img></img>
-      <h3></h3>
-    </div>
-
-    <div>
-      <img></img>
-      <h3></h3>
-    </div>
+<section id="top-five-hl" className="grid grid-cols-1 gap-y-2 px-2">
   
-    <div>
-      <img></img>
-      <h3></h3>
+  {dummyNewsItems.map((news) => (
+    <div key={news.newsId}>
+      <div>
+        <img src={news.image} alt={`Image for ${news.title}`} />
+        <h3>{news.title}</h3>
+      </div>
     </div>
-
-    <div>
-      <img></img>
-      <h3></h3>
-    </div>
-
-   </section>
-       
+  ))}
+   </section>  
   </>
-          );
-          })}
 
-};
+)};
 
 export default Landing;
