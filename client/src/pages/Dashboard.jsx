a9a6666f75a84bc5a46f6c3346ef8a1270ddbb77
Ilir <ilir.hajdari111@gmail.com>
2023-08-24 23:23:39 -0400

    replaced 2d map code with 3d map code, and new token, as well as map style

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,29 @@
-
-import * as React from 'react';
-import Map from 'react-map-gl';
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function Dashboard() {
-  return <Map
-    mapLib={import('mapbox-gl')}
-    initialViewState={{
-      longitude: -74.5,
-      latitude: 40,
-      zoom: 9
-    }}
-    style={{width: 600, height: 400}}
-    mapStyle="mapbox://styles/mapbox/streets-v11"
-    mapboxAccessToken="pk.eyJ1Ijoic3dteXRob3MiLCJhIjoiY2xsbXc5MmE1MDRjMjNla3F6bDhueTV5OSJ9.cu9Y3UeEMkFTX45o0UDaSw"
-  />;
-};
+  const map = useRef(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/swmythos/cllq0tvmh00jm01p9d4q123ar",
+      center: [-74.5, 40],
+      zoom: 9,
+      pitch: 10,
+      bearing: 0,
+      projection: "globe",
+      accessToken:
+        "pk.eyJ1Ijoic3dteXRob3MiLCJhIjoiY2xsbXc5MmE1MDRjMjNla3F6bDhueTV5OSJ9.cu9Y3UeEMkFTX45o0UDaSw",
+    });
+  }, []);
+
+  return (
+    <div ref={mapContainer} style={{ width: "100%", height: "800px" }}></div>
+  );
+}
 
 export default Dashboard;
