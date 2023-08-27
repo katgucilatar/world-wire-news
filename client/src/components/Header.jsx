82ff82e979921a218c92b189057c95321adb2e8e
LJJordan124 <jordan3313.lj@gmail.com>
2023-08-27 13:59:10 -0400

    Frontpage Fix
    The background has been changed back to white so it will be better to see the black text of news articles.
    
    The navbar has been straightened up where the logo is on the left and Login/Signup is on the right.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,21 +6,23 @@ import logo from '../../src/assets/WorldWire-Icon.png'
   const { isLoggedIn, logoutUser } = useCurrentUserContext();
 
   return (
-    <nav className="relative h-14 w-full items-center bg-[#FBFBFB] py-2 px-3 shadow-lg" data-te-navbar-ref>
-      {isLoggedIn() ? (
-        <>
-          <Link to="/dashboard" className="text-blue-600">Dashboard</Link>
-          <button type="button" className="text-blue-600" onClick={logoutUser}>Logout</button>
-        </>
-      ) : (
-        <>
-          <img src={logo} className='absolute w-10 float-left'></img>
-          <div className='float-right py-2'>
-          <Link to="/login" className="text-blue-600 mr-3">Login</Link>
-          <Link to="/register" className="text-blue-600">Sign Up</Link>
-          </div>
-        </>
-      )}
+    <nav className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-2" data-te-navbar-ref>
+      <div className='flex w-full flex-wrap items-center justify-between px-3'>
+        <div><img src={logo} className='w-10'></img></div>
+        {isLoggedIn() ? (
+          <>
+            <Link to="/dashboard" className="text-blue-600">Dashboard</Link>
+            <button type="button" className="text-blue-600" onClick={logoutUser}>Logout</button>
+          </>
+        ) : (
+          <>
+            <div className='py-2'>
+              <Link to="/login" className="text-blue-600 mr-3">Login</Link>
+              <Link to="/register" className="text-blue-600">Sign Up</Link>
+            </div>
+          </>
+        )}
+      </div>
     </nav>
   );
  }
