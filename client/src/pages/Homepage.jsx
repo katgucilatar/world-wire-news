79a98a83cc98b59cc8e3ae480f1fcad6dd415bb0
Herb Sneed <hmsneed@gmail.com>
2023-08-31 17:00:31 -0400

    login register

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -95,6 +95,7 @@ const Homepage = () => {
           w-full
           grid 
           grid-cols-1
+          md:grid-cols-2
           lg:grid-cols-2 
           xl:grid-cols-3
           2xl:grid-cols-4
