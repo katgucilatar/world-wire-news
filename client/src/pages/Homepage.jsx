e18fd8d888635f12417e9071126d41cdf311b68c
Ilir <ilir.hajdari111@gmail.com>
2023-08-31 08:45:21 -0400

    fixing save news issue

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,55 +1,36 @@
-import { useEffect, useState } from 'react';
-
-import { Container, Card, Button, Row, Col } from 'react-bootstrap';
-
-import { useCurrentUserContext } from '../context/CurrentUser';
-
-import Auth from '../utils/auth';
-import { deleteNewsId } from '../utils/localStorage';
-import { useMutation, useQuery } from '@apollo/client';
-import { QUERY_CURRENT_USER } from '../utils/queries';
-import { DELETE_NEWS } from '../utils/mutations';
+import { useEffect, useState } from "react";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { useCurrentUserContext } from "../context/CurrentUser";
+import Auth from "../utils/auth";
+import { deleteNewsId } from "../utils/localStorage";
+import { useMutation, useQuery } from "@apollo/client";
+import { QUERY_CURRENT_USER } from "../utils/queries";
+import { DELETE_NEWS } from "../utils/mutations";
 
 const Homepage = () => {
   const { currentUser } = useCurrentUserContext();
-  console.log(currentUser);
   const { loading, data } = useQuery(QUERY_CURRENT_USER, {
     variables: { email: currentUser.email },
+    fetchPolicy: "network-only",
   });
-  console.log(data);
+
   const userData = data?.currentUser || null;
-  // const [userData, setUserData] = useState(data?.currentUser || null);
   const [deleteNews, { error }] = useMutation(DELETE_NEWS);
-  // useEffect(() => setUserData(data?.currentUser || null), [data]);
 
-  // create function that accepts the news's mongo _id value as param and deletes the news article from the database
-  const handleDeleteNews = async newsId => {
+  const handleDeleteNews = async (newsId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
+    if (!token) return false;
 
     try {
-      const response = await deleteNews({
-        variables: { newsId },
-      });
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // upon success, remove book's id from localStorage
+      const response = await deleteNews({ variables: { newsId } });
+      if (!response.ok) throw new Error("something went wrong!");
       deleteNewsId(newsId);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // if data isn't here yet, say so
-  if (!userData) {
-    return <h2>LOADING...</h2>;
-  }
+  if (loading) return <h2>LOADING...</h2>;
 
   return (
     <>
@@ -62,13 +43,13 @@ const Homepage = () => {
         <h2 className="pt-5">
           {userData?.savedNews.length
             ? `Viewing ${userData.savedNews.length} saved news`
-            : 'You have no saved news!'}
+            : "You have no saved news!"}
         </h2>
         <Row>
-          {userData?.savedNews.map(news => {
+          {userData?.savedNews.map((news) => {
             return (
-              <Col md="4">
-                <Card key={news.newsId} border="dark">
+              <Col md="4" key={news.newsId}>
+                <Card border="dark">
                   {news.image ? (
                     <Card.Img
                       src={news.image}
